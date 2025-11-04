6ff0c44e2a8b9b017ffb9e306dcbc5383f03739b
hernannequi <davidhernannequi@gmail.com>
2025-11-04 12:17:27 -0300

    commit incial

---
+++ mvc-movie-maxiprograma/mvc-movie-maxiprograma/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_movie_maxiprograma.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
