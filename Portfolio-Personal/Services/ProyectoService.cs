8232d4da1f65865782cbacc62f9125da4366486b
hernannequi <davidhernannequi@gmail.com>
2026-05-29 21:44:40 -0300

    Se agregaron proyectos a la seccion correspondiente y se corrigio mapeo de enlaces e imagen de esa seccion

---
+++ Portfolio-Personal/Services/ProyectoService.cs
@@ -0,0 +1,36 @@
+﻿using Portfolio_Personal.Models;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Portfolio_Personal.Services
+{
+    public class ProyectoService
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public ProyectoService(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public List<Proyecto> ObtenerProyectos()
+        {
+            string path = Path.Combine(_env.WebRootPath, "data", "projects.json");
+
+            if (!File.Exists(path))
+                return new List<Proyecto>();
+
+            var json = File.ReadAllText(path);
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            List<Proyecto> jsonDeserialized = JsonSerializer.Deserialize<List<Proyecto>>(json, options)
+                   ?? new List<Proyecto>();
+            
+            return jsonDeserialized;
+        }
+    }
+}
