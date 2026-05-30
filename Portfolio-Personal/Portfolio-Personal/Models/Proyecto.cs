36f74de088f73e2bd949560dc503aaf561147793
hernannequi <davidhernannequi@gmail.com>
2026-05-29 21:34:04 -0300

    Se agregaron proyectos a la seccion correspondiente y se corrigio mapeo de enlaces e imagen de esa seccion

---
+++ Portfolio-Personal/Portfolio-Personal/Models/Proyecto.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio_Personal.Models
+{
+    public class Proyecto
+    {
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public string GithubUrl { get; set; }
+        public string DemoUrl { get; set; }
+        public List<string> Tecnologias { get; set; }
+    }
+}
---
+++ Portfolio-Personal/Portfolio-Personal/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfolio_Personal.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public FormularioContacto Contacto { get; set; } = new();
+        public List<Proyecto> Proyectos { get; set; } = new();
+        public List<ExperienciaLaboral> ExperienciasLaborales { get; set; } = new();
+        public List<FormacionAcademica> FormacionesAcademicas { get; set; } = new();
+    }
+}
